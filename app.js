05839923b50ffaef37aee21b8ef88abc0b473c98
shekharpareekG <shekhar.pareek@sunarctechnologies.com>
2024-02-28 11:11:21 +0530

    code pushing

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ function oauthSignIn() {
     // Parameters to pass to OAuth 2.0 endpoint.
     var params = {
         'client_id': '1085249692225-g6f6fuib5k81c89pav60mhc7hl6pfit2.apps.googleusercontent.com',
-        'redirect_uri': 'https://google-auth-2-0-alpha.vercel.app/', // Update this with your actual redirect URI
+        'redirect_uri': 'http://127.0.0.1:3000/profile.html', // Update this with your actual redirect URI
         'response_type': 'token',
-        'scope': 'https://www.googleapis.com/auth/cloud-platform.read-only',
+        'scope': 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/youtube.readonly',
         'include_granted_scopes': 'true',
         'state': 'pass-through value'
     };
