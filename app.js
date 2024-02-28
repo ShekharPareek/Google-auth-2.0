b8d8e6caee2770498ff4141637fb640cec468250
shekharpareekG <shekhar.pareek@sunarctechnologies.com>
2024-02-28 11:39:41 +0530

    code update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 function oauthSignIn() {
+  document.querySelector(".profile-view").style.display = "block";
     // Google's OAuth 2.0 endpoint for requesting an access token
     var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
   
@@ -9,7 +10,7 @@ function oauthSignIn() {
     // Parameters to pass to OAuth 2.0 endpoint.
     var params = {
         'client_id': '1085249692225-g6f6fuib5k81c89pav60mhc7hl6pfit2.apps.googleusercontent.com',
-        'redirect_uri': 'http://127.0.0.1:3000/profile.html', // Update this with your actual redirect URI
+        'redirect_uri': 'https://google-auth-2-0-alpha.vercel.app/', // Update this with your actual redirect URI
         'response_type': 'token',
         'scope': 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/youtube.readonly',
         'include_granted_scopes': 'true',
@@ -29,3 +30,65 @@ function oauthSignIn() {
     document.body.appendChild(form);
     form.submit();
 }
+
+
+let params = {};
+let regex = /([^&=]+)=([^&]*)/g;
+let m;
+
+while (m = regex.exec(location.href)) {
+    params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+}
+
+if (Object.keys(params).length > 0) {
+    localStorage.setItem('authInfo', JSON.stringify(params));
+}
+
+window.history.pushState({}, document.title, "/" + "profile.html");
+
+let info = JSON.parse(localStorage.getItem('authInfo'));
+console.log(info);
+
+if (info && info.access_token) {
+    fetch('https://www.googleapis.com/oauth2/v3/userinfo', {
+        headers: {
+            "Authorization": `Bearer ${info.access_token}`
+        }
+    })
+    .then((data) => data.json())
+    .then((info) => {
+        console.log(info);
+        document.getElementById("userName").innerHTML += info.name;
+        document.getElementById("userImage").setAttribute('src', info.picture);
+
+    })
+    .catch((error) => {
+        console.error('Error fetching user info:', error);
+    });
+} else {
+    console.error('No access token found in authInfo');
+}
+
+
+
+
+
+
+function signOut() {
+    fetch('https://accounts.google.com/o/oauth2/revoke?token=' + info['access_token'], {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+    .then((response) => {
+        if (response.ok) {
+            localStorage.removeItem('authInfo'); 
+           document.querySelector(".profile-view").style.display = "none";
+        }
+    })
+    .catch((error) => {
+        console.error('Error signing out:', error);
+    });
+}
+
